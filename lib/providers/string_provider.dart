5d741edf949850ffda6475dfa21ecf9422fac24c
Kyle <hopeman1986@gmail.com>
2020-07-19 22:22:33 +0200

    added Provider

--- lib/providers/string_provider.dart
+++ lib/providers/string_provider.dart
@@ -1,13 +1,15 @@
 library modelmixer;
 
 import 'package:lipsum/lipsum.dart';
+import 'package:modelmixer/providers/provider.dart';
 
-class StringProvider {
+class StringProvider implements Provider<String> {
   StringProvider({this.numWords = defaultValue});
 
   static const int defaultValue = 1;
   int numWords;
 
+  @override
   String get() {
     if (numWords == 0) {
       print('$numWords is not a valid input, reverting to default $defaultValue');
