1a1b9aada912d7310a406c72ef32bf4635841438
Kyle <hopeman1986@gmail.com>
2020-07-19 22:16:18 +0200

    updated tests

--- lib/providers/string_provider.dart
+++ lib/providers/string_provider.dart
@@ -3,11 +3,16 @@ library modelmixer;
 import 'package:lipsum/lipsum.dart';
 
 class StringProvider {
-  StringProvider({this.numWords = 1});
+  StringProvider({this.numWords = defaultValue});
 
+  static const int defaultValue = 1;
   int numWords;
 
   String get() {
+    if (numWords == 0) {
+      print('$numWords is not a valid input, reverting to default $defaultValue');
+      numWords = defaultValue;
+    }
     return createWord(numWords: numWords);
   }
 }
--- test/providers/string_provider_test.dart
+++ test/providers/string_provider_test.dart
@@ -3,22 +3,29 @@ import 'package:test/test.dart';
 import 'package:modelmixer/providers/string_provider.dart';
 
 void main() {
-  group('Test get()', () {
-    test('default', () {
-      var generated = StringProvider().get();
-      expect(String, generated.runtimeType);
-      expect(true, generated.isNotEmpty);
-      expect(1, generated.wordCount());
-    });
+  test('test default', () {
+    var generated = StringProvider().get();
+    expect(String, generated.runtimeType);
+    expect(true, generated.isNotEmpty);
+    expect(1, generated.wordCount());
+  });
 
-    // TODO: parameterized testing
-    test('multiple words', () {
-      var generated = StringProvider(numWords: 3).get();
+  test('test input 0', () {
+    var generated = StringProvider(numWords: 0).get();
+    expect(String, generated.runtimeType);
+    expect(true, generated.isNotEmpty);
+    expect(1, generated.wordCount());
+  });
 
-      expect(String, generated.runtimeType);
-      expect(true, generated.isNotEmpty);
-      expect(3, generated.wordCount());
-    });
+  group("test input", () {
+    for (int i = 1; i < 100; i++) {
+      var generated = StringProvider(numWords: i).get();
+      test('given: $i expected: $i', () {
+        expect(String, generated.runtimeType);
+        expect(true, generated.isNotEmpty);
+        expect(i, generated.wordCount());
+      });
+    }
   });
 }
 
