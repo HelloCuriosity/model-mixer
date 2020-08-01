3993f730c5e2e3a60da3375448df7c83e4ec7d57
Kyle <kyle.roe@thermondo.de>
2020-08-01 08:50:16 +0200

    [Quality Gate] Removed Lint Warnings

--- test/model_mixer_test.dart
+++ test/model_mixer_test.dart
@@ -4,51 +4,51 @@ import 'package:model_mixer/model_mixer.dart';
 import 'package:test/test.dart';
 
 void main() {
-  ModelMixer modelMixer = ModelMixer();
+  final ModelMixer modelMixer = ModelMixer();
 
   test("test build", () {
-    TestObject testObject = modelMixer.build(TestObject);
+    final TestObject testObject = modelMixer.build(TestObject);
     testObject.verify();
   });
 
   group("test buildList", () {
     test("default", () {
-      List<TestObject> list = modelMixer.buildList(TestObject);
+      final List<TestObject> list = modelMixer.buildList(TestObject);
       expect(10, list.length);
-      list.forEach((testObject) {
-        testObject.verify();
-      });
+      for (var item in list) {
+        item.verify();
+      }
     });
 
     for (int i = 1; i < 100; i++) {
       test('given: $i expected: $i', () {
-        List<TestObject> list = modelMixer.buildList(TestObject, i);
+        final List<TestObject> list = modelMixer.buildList(TestObject, i);
         expect(i, list.length);
-        list.forEach((testObject) {
-          testObject.verify();
-        });
+        for (var item in list) {
+          item.verify();
+        }
       });
     }
   });
 
   group("test arguments", () {
-    ClassMirror mirror = reflectClass(TestObject);
+    final ClassMirror mirror = reflectClass(TestObject);
 
     test("test method", () {
-      List<dynamic> arguments = ModelMixer.getArguments(mirror);
+      final List<dynamic> arguments = ModelMixer.getArguments(mirror);
       arguments.verify();
     });
 
     test("test extension", () {
-      List<dynamic> arguments = mirror.arguments();
+      final List<dynamic> arguments = mirror.arguments();
       arguments.verify();
     });
   });
 }
 
 extension _ExpectExt on List<dynamic> {
-  verify() {
-    expect(3, this.length);
+  void verify() {
+    expect(3, length);
     expect(String, this[0].runtimeType);
     expect(int, this[1].runtimeType);
     expect(double, this[2].runtimeType);
@@ -64,9 +64,9 @@ class TestObject {
 }
 
 extension _TestObjectExt on TestObject {
-  verify() {
-    expect(String, this.stringValue.runtimeType);
-    expect(int, this.intValue.runtimeType);
-    expect(double, this.doubleValue.runtimeType);
+  void verify() {
+    expect(String, stringValue.runtimeType);
+    expect(int, intValue.runtimeType);
+    expect(double, doubleValue.runtimeType);
   }
 }
